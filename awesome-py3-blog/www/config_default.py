8527a23a4cefb38998e2d2b38719bb59de880463
slimshaco <287168755@qq.com>
2017-02-23 23:36:46 +0800

    delete data

--- awesome-py3-blog/www/config_default.py
+++ awesome-py3-blog/www/config_default.py
@@ -3,12 +3,12 @@
 configs = {
 	'db':{
 		'host': '127.0.0.1',
-		'port': 3306,
-		'user': 'hpf',
-		'password': '8688680',
-		'db': 'slimshaco',
+		'port': 3,
+		'user': '',
+		'password': '',
+		'db': '',
 	},
 	'session':{
-		'secret':'HPFBlOg'
+		'secret':''
 	}
 }
